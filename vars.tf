2c599b9a10dfb142f47f3a0595d7aa549d52b051
Manjari4 <98511353+Manjari4@users.noreply.github.com>
2023-07-20 18:52:04 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -5,35 +5,3 @@ variable "resource_group_name" {
 variable "resource_group_location" {
   type = string
 }
-
-variable "container_group_name" {
-  type = string
-}
-
-variable "restart_policy" {
-  type = string
-}
-
-variable "container_name" {
-  type = string
-}
-
-variable "cpu_cores" {
-  type        = number
-  description = "The number of CPU cores to allocate to the container."
-  default     = 1
-}
-
-variable "memory_in_gb" {
-  type        = number
-  description = "The amount of memory to allocate to the container in gigabytes."
-  default     = 2
-}
-
-variable "image" {
-  type = string
-}
-
-variable "port" {
-  type = string
-}
